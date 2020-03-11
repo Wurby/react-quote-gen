3094dec39a755622013495e5f5e0cdd70101f881
Joshua Pearson <wurby@Joshuas-MacBook-Air.local>
2020-03-10 22:36:27 -0600

    Updates

---
+++ precache-manifest.8b77144a40787ea700f106659428d3a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe50a1309c2012a781d4791b19d71676",
+    "url": "/react-quote-gen/index.html"
+  },
+  {
+    "revision": "30503ad6e05c013c0dc5",
+    "url": "/react-quote-gen/static/css/main.5e284e3c.chunk.css"
+  },
+  {
+    "revision": "fa28b3474626e1f87e16",
+    "url": "/react-quote-gen/static/js/2.54cb113f.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react-quote-gen/static/js/2.54cb113f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30503ad6e05c013c0dc5",
+    "url": "/react-quote-gen/static/js/main.5d42ad99.chunk.js"
+  },
+  {
+    "revision": "a70408f68e5707149b3c",
+    "url": "/react-quote-gen/static/js/runtime-main.c1dd83fe.js"
+  }
+]);
